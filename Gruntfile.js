4864f84c571094da18eeb15c1df348c1d120f023
Max Quattromani <max.quattromani@twcable.com>
2014-02-21 22:26:26 -0700

    added grunt setup

---
+++ Gruntfile.js
@@ -0,0 +1,84 @@
+module.exports = function(grunt) {
+
+    // load all grunt tasks matching the `grunt-*` pattern
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+
+      concat: {
+        dist: {
+          src: [
+            'js/*.js'
+          ],
+          dest: 'js/build/production.js',
+        }
+      },
+
+      uglify: {
+        build: {
+          src: 'js/build/production.js',
+          dest: 'js/build/production.min.js'
+        }
+      },
+
+      sass: {
+        dist: {
+          options: {
+            style: 'expanded'
+          },
+          files: [{
+            expand: true,
+            cwd: 'css/scss/partials',
+            src: ['*.scss'],
+            dest: 'css/',
+            ext: '.css'
+          }], 'main.css': 'main.scss',
+        },
+        dev: {
+          files: [{'css/main.css': 'css/scss/main.scss'}]
+        }
+      },
+
+      cssmin: {
+        combine: {
+          files: {
+            'css/main.min.css': [ 'css/main.css' ]
+          }
+        }
+      },
+
+      imagemin: {
+        dynamic: {
+          files: [{
+            expand: true,
+            cwd: 'images/orig_assets',
+            src: ['*.{png,jpg,gif}'],
+            dest: 'images'
+          }]
+        }
+      },
+
+      watch: {
+        scripts: {
+          files: ['js/*.js'],
+          tasks: ['concat', 'uglify'],
+          options: {
+            spawn: false,
+          }
+        },
+        css: {
+          files: ['css/scss/partials/*.scss','css/scss/modules/*.scss'],
+          tasks: ['sass', 'cssmin'],
+          options: {
+            spawn: false,
+          }
+        },
+      }
+
+    });
+
+    grunt.registerTask('production', ['concat', 'uglify', 'sass', 'cssmin', 'imagemin', 'watch']);
+    grunt.registerTask('dev', ['concat', 'uglify', 'sass', 'cssmin', 'watch']);
+
+};
