e4439f6d000c34ebfab9edfa2f32287b9897fc41
Max Quattromani <max.quattromani@twcable.com>
2014-02-23 09:06:41 -0700

    update to grid structure

--- styleguide/grid/index.php
+++ styleguide/grid/index.php
@@ -2,156 +2,138 @@
 <a class="btn vs right" href="#">View Source</a>
 <div class="box-column clear">
 
-    <div class="vertical lined">
-        <ul>
-            <li>
-                <div class="row text-center">
-                    <div class="column grid full color-primary-blue text-white">
-                    </div>
-                </div>
-                <div class="row text-center">
-                    <div class="column grid half color-primary-blue text-white">
-                    </div>
-                    <div class="column grid half color-primary-blue text-white">
-                    </div>
-                </div>
-
-                <pre class="prettyprint">
-                    &lt;div class="row"&gt;
-                    &lt;div class="column half"&gt;
-
-                    &lt;/div&gt;
-                    &lt;div class="column half"&gt;
-
-                    &lt;/div&gt;
-                    &lt;/div&gt;
-                </pre>
-
-                <div class="row text-center">
-                    <div class="column grid third color-primary-blue text-white">
-                    </div>
-                    <div class="column grid third color-primary-blue text-white">
-                    </div>
-                    <div class="column grid third color-primary-blue text-white">
-                    </div>
-                </div>
-
-                <div class="row text-center">
-                    <div class="column grid fourth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid fourth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid fourth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid fourth color-primary-blue text-white">
-                    </div>
-                </div>
-
-                <div class="row text-center">
-                    <div class="column grid fifth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid fifth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid fifth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid fifth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid fifth color-primary-blue text-white">
-                    </div>
-                </div>
-
-                <div class="row text-center">
-                    <div class="column grid sixth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid sixth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid sixth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid sixth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid sixth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid sixth color-primary-blue text-white">
-                    </div>
-                </div>
-                <div class="row text-center">
-                    <div class="column grid eighth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid eighth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid eighth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid eighth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid eighth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid eighth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid eighth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid eighth color-primary-blue text-white">
-                    </div>
-                </div>
-                <div class="row text-center">
-                    <div class="column grid tenth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid tenth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid tenth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid tenth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid tenth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid tenth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid tenth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid tenth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid tenth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid tenth color-primary-blue text-white">
-                    </div>
-                </div>
-
-                <div class="row text-center">
-                    <div class="column grid twelfth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid twelfth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid twelfth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid twelfth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid twelfth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid twelfth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid twelfth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid twelfth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid twelfth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid twelfth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid twelfth color-primary-blue text-white">
-                    </div>
-                    <div class="column grid twelfth color-primary-blue text-white">
-                    </div>
-                </div>
-            </li>
-            <li>
-                <?php if ($about) { include('combinations/index.php'); } ?>
-            </li>
-            <li>
-                <?php if ($about) { include('marginless/index.php'); } ?>
-            </li>
-            <li>
-                <?php if ($about) { include('marginless/combinations/index.php'); } ?>
-            </li>
-        </ul>
+    <div class="row">
+        <div class="full grid"></div>
     </div>
 
-</div>
+    <div class="row">
+        <div class="halfs grid"></div>
+        <div class="halfs grid"></div>
+    </div>
+
+    <div class="row">
+        <div class="thirds grid"></div>
+        <div class="thirds grid"></div>
+        <div class="thirds grid"></div>
+    </div>
+
+    <div class="row">
+        <div class="fourths grid"></div>
+        <div class="fourths grid"></div>
+        <div class="fourths grid"></div>
+        <div class="fourths grid"></div>
+    </div>
+
+    <div class="row">
+        <div class="fifths grid"></div>
+        <div class="fifths grid"></div>
+        <div class="fifths grid"></div>
+        <div class="fifths grid"></div>
+        <div class="fifths grid"></div>
+    </div>
+
+    <div class="row">
+        <div class="sixths grid"></div>
+        <div class="sixths grid"></div>
+        <div class="sixths grid"></div>
+        <div class="sixths grid"></div>
+        <div class="sixths grid"></div>
+        <div class="sixths grid"></div>
+    </div>
+
+    <div class="row">
+        <div class="sevenths grid"></div>
+        <div class="sevenths grid"></div>
+        <div class="sevenths grid"></div>
+        <div class="sevenths grid"></div>
+        <div class="sevenths grid"></div>
+        <div class="sevenths grid"></div>
+        <div class="sevenths grid"></div>
+    </div>
+
+    <div class="row">
+        <div class="eighths grid"></div>
+        <div class="eighths grid"></div>
+        <div class="eighths grid"></div>
+        <div class="eighths grid"></div>
+        <div class="eighths grid"></div>
+        <div class="eighths grid"></div>
+        <div class="eighths grid"></div>
+        <div class="eighths grid"></div>
+    </div>
+
+    <div class="row">
+        <div class="ninths grid"></div>
+        <div class="ninths grid"></div>
+        <div class="ninths grid"></div>
+        <div class="ninths grid"></div>
+        <div class="ninths grid"></div>
+        <div class="ninths grid"></div>
+        <div class="ninths grid"></div>
+        <div class="ninths grid"></div>
+        <div class="ninths grid"></div>
+    </div>
+
+    <div class="row">
+        <div class="tenths grid"></div>
+        <div class="tenths grid"></div>
+        <div class="tenths grid"></div>
+        <div class="tenths grid"></div>
+        <div class="tenths grid"></div>
+        <div class="tenths grid"></div>
+        <div class="tenths grid"></div>
+        <div class="tenths grid"></div>
+        <div class="tenths grid"></div>
+        <div class="tenths grid"></div>
+    </div>
+
+    <div class="row">
+        <div class="elevenths grid"></div>
+        <div class="elevenths grid"></div>
+        <div class="elevenths grid"></div>
+        <div class="elevenths grid"></div>
+        <div class="elevenths grid"></div>
+        <div class="elevenths grid"></div>
+        <div class="elevenths grid"></div>
+        <div class="elevenths grid"></div>
+        <div class="elevenths grid"></div>
+        <div class="elevenths grid"></div>
+        <div class="elevenths grid"></div>
+    </div>
+
+    <div class="row">
+        <div class="twelfths grid"></div>
+        <div class="twelfths grid"></div>
+        <div class="twelfths grid"></div>
+        <div class="twelfths grid"></div>
+        <div class="twelfths grid"></div>
+        <div class="twelfths grid"></div>
+        <div class="twelfths grid"></div>
+        <div class="twelfths grid"></div>
+        <div class="twelfths grid"></div>
+        <div class="twelfths grid"></div>
+        <div class="twelfths grid"></div>
+        <div class="twelfths grid"></div>
+    </div>
+
+    <div class="row">
+        <div class="halfs grid">
+
+        </div>
+        <div class="fourths grid"></div>
+        <div class="fourths grid"></div>
+    </div>
+
+    <div class="row">
+        <div class="three-fourths grid">
+
+        </div>
+        <div class="fourths grid"></div>
+    </div>
+
+    <div class="row">
+        <div class="three-eighths grid">
+
+        </div>
+        <div class="five-eighths grid"></div>
+    </div>
