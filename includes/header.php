f3325e9ba46911ef19fb3e8f45f74b79c9f2037a
Max Quattromani <max.quattromani@twcable.com>
2014-02-26 16:13:35 -0700

    repair relative path of header.php

--- includes/header.php
+++ includes/header.php
@@ -3,11 +3,11 @@
 // Include all needed template pieces
 
 	// Load Header -- includes TopHat
-		include_once($project_title .'includes/head.php');
+		include_once($project_root .'includes/head.php');
 
 	// Load Content opening tags
 	if ($content == true) {
-		include_once($project_title .'includes/content.php');
+		include_once($project_root .'includes/content.php');
 	}
 
 ?>
